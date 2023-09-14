9d35f7ce2b86eb00dd3cc7a70a9773506117de39
kalchurrrrr <nikolaykalchu@gmail.com>
2023-09-14 21:26:15 +0300

    Initial commit

---
+++ src/main/java/com/coursework/secondCoursework/SecondCourseworkApplication.java
@@ -0,0 +1,13 @@
+package com.coursework.secondCoursework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondCourseworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondCourseworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coursework/secondCoursework/SecondCourseworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coursework.secondCoursework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondCourseworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
